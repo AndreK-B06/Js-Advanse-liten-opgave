36693a77d51cbdd67f94564bdb8473dc9204628b
AndreK-B06 <andrekb06.kodehode@gmail.com>
2023-09-25 13:49:56 +0200

    pading fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const quote = [
-  "The only thing necessary for the triumph of evil is for good men to do nothing. - Edmund Burke",
-  "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-  "Life is what happens when you're busy making other plans. - John Lennon",
-  "The best way to predict the future is to create it. - Peter Drucker",
-  "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. - Ralph Waldo Emerson",
+  "The only thing necessary for the triumph of evil is for good men to do nothing. - Edmund Burke.",
+  "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill.",
+  "Life is what happens when you're busy making other plans. - John Lennon.",
+  "The best way to predict the future is to create it. - Peter Drucker.",
+  "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. - Ralph Waldo Emerson.",
 ];
 /* ---------------------
 !!! Get HTML Eliment !!!
